6b229c8603d337cb67c403adb408ec1ff28a183f
JaumVituu <vitor40joao@gmail.com>
2021-03-12 15:42:38 -0300

    Edição dos arquivos
    (Não sei se está funcionando ou não, logo que ao apertar 'Jogar" não acontece nada)

---
+++ javascript/Constantes.js
@@ -0,0 +1,19 @@
+'use strict';
+const COLUNA = 10;
+const LINHA = 20;
+const TAM_BLOCO = 30;
+
+const KEY = {
+	LEFT: 37,
+	RIGHT: 39,
+	DOWN: 40,
+	SPACE: 32
+	
+}
+Object.freeze(KEY);
+
+const movimento = {
+	[KEY.LEFT] p => ({ ...p, x: p.x - 1 }),
+	[KEY.RIGHT]  p => ({ ...p, x: p.x + 1 }),
+	[KEY.DOWN]  p => ({ ...p, x: p.y + 1 }),
+};
---
+++ javascript/pecas.js
@@ -0,0 +1,34 @@
+class Peca {
+	constructor(ctx) {
+		this.ctx = ctx;
+		this.color = 'blue';
+		this.shape = [
+		    [2, 0, 0],
+			[2, 2, 2],
+			[0, 0, 0]
+		];
+		
+		//Posição inicial no painel.
+		this.x = 3;
+		this.y = 0;
+	}
+	desenho() {
+	  //define a cor de preenchimento
+this.ctx.fillStyle = this.color;
+this.shape.forEach((row, y) => {
+	row.forEach((value, x) => {
+		     //this.x, this.y determinam a posição esquerda
+		     //x, y determina a posição do bloco na forma
+		     //this.x + x é a posição do bloco no painel
+		     if (value > 0) {
+			     //preenche a forma
+			     this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+		    }
+	    });
+    });
+}
+mover(p) {
+	this.x = p.x;
+	this.y = p.y;
+}
+
